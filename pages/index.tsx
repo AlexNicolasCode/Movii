c9b4e5ed6eebcef6f4d752e8b37378245ef58a9d
Alex <alexoliveira604@gmail.com>
2021-07-24 00:09:46 -0300

    pagination added in home

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,40 +4,88 @@ import { Movie } from '../src/components/movie/movie'
 import { GetStaticProps } from 'next'
 import { api } from '../src/services/api'
 
-import { AllCategories, Category, OptionsCategories } from '../src/components/styles/styles'
+import { useEffect, useState } from 'react';
 import { Gender } from '../src/components/categories/gender'
-import styles from '../styles/Home.module.scss'
-import { useState } from 'react';
 import { useFilter } from '../src/contexts/filter';
+
 import { FilterModel } from '../src/components/filter';
+import { AllCategories, Category, OptionsCategories } from '../src/components/styles/styles'
+import styles from '../styles/Home.module.scss'
+import { Pagination } from '../src/components/pagination';
 
 type MovieData = {
   id: string,
+  name: string,
   title: string,
   vote_average: string,
-  poster_path: string
+  poster_path: string,
+  genre_ids: [
+    genre: number[]
+  ]
 }
 
 type Genres = {
   id: number,
-  name: string  
+  name: string,
 }
 
 type HomeProps = {
   allMovies: MovieData[],
-  genres: Genres[]
+  genres: Genres[],
+  slug: number
 }
 
 export default function Home({ allMovies, genres }: HomeProps) {
   const [ categories, setCategories ] = useState<boolean>(false)
+  const [ filters, setFilters ] = useState<JSX.Element[]>([])
+  const [ currentMovies, setCurrentMovies ] = useState<MovieData[]>([])
   const { currentFiltersData } = useFilter()
 
-  const toggleCaregoriesButtonStatus = () => {
-    if (categories === true) {
-      setCategories(false);
-    } else {
-      setCategories(true);
+  useEffect(() => {
+    const getAllMovies = () => {
+      currentFiltersData === undefined ? setCurrentMovies(allMovies) : getFiltersProps()
     }
+
+    console.log(currentFiltersData)
+    getAllMovies()
+  }, [])
+
+  useEffect(() => {
+    const getCurrentFilter = async () => {
+      if (currentFiltersData.length > 0) {
+        const categoriesFilters = await currentFiltersData.map((filter, index) => {
+          return <FilterModel key={index} name={filter.name}/>
+        })
+        
+        setFilters(categoriesFilters)
+      }
+    }
+
+    getFiltersProps()
+    getCurrentFilter()
+  }, [currentFiltersData])
+
+  const getFiltersProps = async () => {        
+    if (currentFiltersData.length > 0) {
+      const categoriesFiltedIds = await currentFiltersData.map((filter) => {
+        return filter.id
+      });
+  
+      const categoriesFilters = await currentFiltersData.map((filter, index) => {
+        return <FilterModel key={index} name={filter.name}/>
+      });
+
+      const movieListFilted = await allMovies.filter(movie => {
+        return movie.genre_ids.find(genre => { return genre == categoriesFiltedIds});
+      })
+
+      setFilters(categoriesFilters)
+      setCurrentMovies(movieListFilted) 
+    } else setCurrentMovies(allMovies)
+  }
+
+  const toggleCaregoriesButtonStatus = () => {
+    categories === true ? setCategories(false) : setCategories(true)
   }
 
   return (
@@ -51,20 +99,17 @@ export default function Home({ allMovies, genres }: HomeProps) {
 
       <header>
         <section>
-          {currentFiltersData.map((filter, index: number) => {
-              return <FilterModel index={index} name={filter.name}/>
-            })
-          }
+          {filters}
         </section>
 
         <section>
           <OptionsCategories onClick={toggleCaregoriesButtonStatus}>
             Categories
           </OptionsCategories>
-          { categories &&
+          {categories &&
             <AllCategories>
-              {genres.map(gender => {
-                return <Gender name={gender.name} />
+              {genres.map((gender, index) => {
+                return <Gender key={index} id={gender.id} name={gender.name} />
               })}
             </AllCategories>
           }
@@ -75,16 +120,17 @@ export default function Home({ allMovies, genres }: HomeProps) {
         <Category>The Most Popular Movies</Category>
 
         <section>
-          {allMovies.map((movie) => {
+          {currentMovies.map((movie, index) => {
             return (
               <>
-                <Link key={movie.id} href={`/movie/${movie.id}`}>
-                  {movie.title}
-                </Link>
-                <Movie id={movie.id} rate={movie.vote_average} image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                <Movie key={index} id={movie.id} rate={movie.vote_average} image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
               </>
             )
           })}
+        </section>
+
+        <section>
+          <Pagination page_id={"1"}/>
         </section>
       </main>
 
@@ -98,16 +144,16 @@ export default function Home({ allMovies, genres }: HomeProps) {
 export const getStaticProps: GetStaticProps = async () => {
   const { data } = await api.get('movie/popular?api_key=c87d684e83e180236e81d0dae298e88c', {
     params: {
-      _limit: 12,
+      _limit: 20,
       _sort: 'published_at',
       _order: 'desc'
     }
   })
-
-  const genresData = await api.get('genre/movie/list?api_key=c87d684e83e180236e81d0dae298e88c')
-  const genres = genresData.data.genres
-
-  const allMovies = data.results
+  
+  const genresData = await api.get('genre/movie/list?api_key=c87d684e83e180236e81d0dae298e88c');
+  const genres = genresData.data.genres;
+  
+  const allMovies = data.results;
 
   return {
     props: {
